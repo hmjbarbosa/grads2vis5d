20cde1a60ef151ec55ff0b56a0caf1f85dda8c6c
Henrique de Melo Jorge Barbosa <hbarbosa@samsung>
2011-08-22 01:18:35 -0300

    Makefile with gcc/gfotran, now working

---
+++ getch.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<termios.h>
+#include<unistd.h>
+
+void getch_(char *ch)
+{
+        struct termios oldt,
+                       newt;
+
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~ ( ICANON | ECHO );
+        tcsetattr( STDIN_FILENO, TCSANOW, &newt);
+        *ch = getchar();
+        tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
+}
